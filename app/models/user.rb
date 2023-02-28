b8dffe7be6e4b285f02e3212cb35e4ca133b4718
日下陸矢 <rikuya.98@gmail.com>
2023-02-28 21:44:26 +0900

    ・プロフィールのformの作成 ・アクティブレコードの導入 ・プロフィールページのデザイン作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :profile, dependent: :destroy
+
+  delegate :birthday, :gender, to: :profile, allow_nil: true
+  
+  def prepare_profile
+    profile || build_profile
+  end
+
+
+  def display_name
+    profile&.nickname || self.email.split('@').first
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+     profile.avatar
+    else
+     'default-avatar.png'
+    end
+  end
+
+
 end
