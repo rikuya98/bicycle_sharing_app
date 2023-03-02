f2c8b864bb94c8528f8a9b6e3493afa8c5b0b61b
日下陸矢 <rikuya.98@gmail.com>
2023-03-02 10:48:16 +0900

    ・articleモデルの作成 ・article_contorollerの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,44 @@
 class ArticlesController < ApplicationController
     def index
     end
+
+    def new
+        @article = current_user.articles.build
+    end
+
+    def create
+        @article = current_user.articles.build(article_params)
+        if @article.save
+          redirect_to article_path(@article), notice: '記事が投稿されました'
+        else
+          flash.now[:error] = '記事が投稿できませんでした'
+          render :new
+        end
+    end
+
+    def edit
+        @article = current?user.articles.find(params[:id])
+    end
+
+    def update
+        @article = current_user.articles.find(params[:id])
+        if @article.update(article_params)
+            redirect_to article_path(@article), notice: '更新できました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    def destroy
+        article = current_user.articles.find(params[:id])
+        article.destroy!
+        redirect_to root_path, notice: '記事が削除されました'
+    end
+
+    private
+    
+    def article_params
+        params.require(:article).permit(:title, :content, :eyecatch)
+    end
 end
