9ecd331381841125df81488f725ea1a37d16f587
日下陸矢 <rikuya.98@gmail.com>
2023-03-07 07:41:19 +0900

    tagモデルの作成　タグと記事を中間テーブルで紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
 
+  has_many :article_tags
+  has_many :tags, through: :article_tags
   has_many_attached :images
 
   def article_image(image)
