398f87b491f54bff57ccf03cbddac12830ebfa41
日下陸矢 <rikuya.98@gmail.com>
2023-03-27 22:54:48 +0900

    コメントアウトの削除

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -3,12 +3,3 @@ window.updateCommentsArea = function(commentsArea, comments) {
     document.querySelector("textarea").value = "";
   };
   
-
-
-
-// function updateCommentsArea(comments) {
-//     // 関数内で変数を定義
-//     const commentsArea = document.getElementById("comments");
-//     commentsArea.innerHTML = comments;
-//     document.querySelector("textarea").value = "";
-//   }
