1710cec4a3a4e962b111e44960c481400b33c44f
日下陸矢 <rikuya.98@gmail.com>
2023-03-31 09:00:51 +0900

    searchのviewを作成 コース検索のformの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
   resources :articles do
+    collection do
+    get 'search'
     resources :comments, only: [:destroy, :create]
     resource :like, only: [:show, :create, :destroy]
+    end
   end
 
   resource :profile, only: [:show, :edit, :update]
